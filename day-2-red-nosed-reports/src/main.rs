a3bf920e768dbf5903fd7a7ebc0106d1c0d54e6e
Koen Klinkers <k.klinkers@gmail.com>
2024-12-02 17:15:39 +0100

    day-2

---
+++ day-2-red-nosed-reports/src/main.rs
@@ -0,0 +1,13 @@
+use toolkit::input::get_input;
+
+fn main() {
+    let contents = get_input();
+    println!("{}", contents);
+}
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_2_red_nosed_reports() {
+        assert_eq!(1, 1)
+    }
+}
