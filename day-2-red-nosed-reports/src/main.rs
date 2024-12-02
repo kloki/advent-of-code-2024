37d368788716f511b02140b21edc8f5f29267103
Koen Klinkers <k.klinkers@gmail.com>
2024-12-02 17:25:12 +0100

    wip

--- day-2-red-nosed-reports/src/main.rs
+++ day-2-red-nosed-reports/src/main.rs
@@ -4,10 +4,26 @@ fn main() {
     let contents = get_input();
     println!("{}", contents);
 }
+
+pub fn is_safe(input: Vec<isize>) -> bool {
+    let diffs: Vec<isize> = input.windows(2).map(|d| d[0] - d[1]).collect();
+    diffs
+        .iter()
+        .filter(|x| *x < &3 && *x > &0)
+        .collect::<Vec<_>>()
+        .is_empty()
+        || diffs
+            .iter()
+            .filter(|x| *x > &-3 && *x < &0)
+            .collect::<Vec<_>>()
+            .is_empty()
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn test_2_red_nosed_reports() {
-        assert_eq!(1, 1)
+        assert!(is_safe(vec![7, 6, 4, 2, 1]));
     }
 }
