c4d14919dc9244b9068f8661ca62e0b5d42d26c4
Koen Klinkers <k.klinkers@gmail.com>
2024-12-02 18:11:46 +0100

    first complete

--- day-2-red-nosed-reports/src/main.rs
+++ day-2-red-nosed-reports/src/main.rs
@@ -1,22 +1,30 @@
 use toolkit::input::get_input;
 
-fn main() {
-    let contents = get_input();
-    println!("{}", contents);
+fn parse_input(input: String) -> Vec<Vec<isize>> {
+    input
+        .trim()
+        .split("\n")
+        .map(|line| {
+            line.split(" ")
+                .map(|i| i.parse::<isize>().unwrap())
+                .collect()
+        })
+        .collect()
 }
 
-pub fn is_safe(input: Vec<isize>) -> bool {
+fn main() {
+    let contents = get_input();
+    let reports = parse_input(contents);
+
+    println!(
+        "Soluction 1: {}",
+        reports.iter().filter(|x| is_safe(x)).count()
+    )
+}
+
+pub fn is_safe(input: &Vec<isize>) -> bool {
     let diffs: Vec<isize> = input.windows(2).map(|d| d[0] - d[1]).collect();
-    diffs
-        .iter()
-        .filter(|x| *x < &3 && *x > &0)
-        .collect::<Vec<_>>()
-        .is_empty()
-        || diffs
-            .iter()
-            .filter(|x| *x > &-3 && *x < &0)
-            .collect::<Vec<_>>()
-            .is_empty()
+    diffs.iter().all(|x| *x < 4 && *x > 0) || diffs.iter().all(|x| *x > -4 && *x < 0)
 }
 
 #[cfg(test)]
@@ -24,6 +32,11 @@ mod tests {
     use super::*;
     #[test]
     fn test_2_red_nosed_reports() {
-        assert!(is_safe(vec![7, 6, 4, 2, 1]));
+        assert!(is_safe(&vec![7, 6, 4, 2, 1]));
+        assert!(!is_safe(&vec![1, 2, 7, 8, 9]));
+        assert!(!is_safe(&vec![9, 7, 6, 2, 1]));
+        assert!(!is_safe(&vec![1, 3, 2, 4, 5]));
+        assert!(!is_safe(&vec![8, 6, 4, 4, 1]));
+        assert!(is_safe(&vec![1, 3, 6, 7, 9]));
     }
 }
