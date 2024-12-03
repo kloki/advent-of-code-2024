931dd8323dc43f6f2808f3110759b7150c7a5c8d
Koen Klinkers <k.klinkers@gmail.com>
2024-12-03 12:09:41 +0100

    not correct

--- day-3-mull-it-over/src/main.rs
+++ day-3-mull-it-over/src/main.rs
@@ -3,7 +3,11 @@ use toolkit::input::get_input;
 
 fn main() {
     let contents = get_input();
-    println!("Solution 1 {}", calculate_complete_mulls(contents));
+    println!("Solution 1 {}", calculate_complete_mulls(contents.clone()));
+    println!(
+        "Solution 2 {}",
+        calculate_complete_mulls_conditional(contents)
+    );
 }
 
 #[derive(Debug)]
@@ -35,14 +39,57 @@ pub fn calculate_complete_mulls(input: String) -> usize {
     mulls.iter().map(|x| x.calc()).sum()
 }
 
+pub fn prune_input(input: String) -> Vec<String> {
+    let mut work_input = input;
+    let mut result = Vec::new();
+    loop {
+        match work_input.split_once("don't") {
+            Some((included, remainder)) => {
+                result.push(included.to_string());
+                work_input = remainder.to_string()
+            }
+            None => {
+                result.push(work_input);
+                break;
+            }
+        }
+
+        match work_input.split_once("do") {
+            Some((_, remainder)) => work_input = remainder.to_string(),
+            None => {
+                break;
+            }
+        }
+    }
+    result
+}
+
+pub fn calculate_complete_mulls_conditional(input: String) -> usize {
+    prune_input(input)
+        .iter()
+        .map(|line| calculate_complete_mulls(line.to_string()))
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    const TEST_INPUT: &str =
+    const TEST_INPUT_1: &str =
         "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+
+    const TEST_INPUT_2: &str =
+        "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
     #[test]
     fn test_1_mull_it_over() {
-        assert_eq!(calculate_complete_mulls(TEST_INPUT.to_string()), 161)
+        assert_eq!(calculate_complete_mulls(TEST_INPUT_1.to_string()), 161)
+    }
+
+    #[test]
+    fn test_2_mull_it_over() {
+        assert_eq!(
+            calculate_complete_mulls_conditional(TEST_INPUT_2.to_string()),
+            48
+        )
     }
 }
